3aaf0b1c67115230461ce86fe2f086ffbdd89cfe
Jonah Johansen <johansenjonah+git@gmail.com>
2021-01-25 11:52:38 -0800

    begin prototyping ToDoList business logic

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function ToDoList() {
+  this.items = {};
+  this.currentId = 0;
+}
+
+function Item(name, desc) {
+  this.name = name;
+  this.desc = desc;
+  this.done = false;
+}
+
+ToDoList.prototype.addItem = function(item) {
+  item.id = this.assignId();
+  this.items[item.id] = item;
+}
+
+ToDoList.prototype.assignId = function() {
+  sendId = this.currentId
+  this.currentId += 1;
+  return sendId;
+}
+
+let firstList = new ToDoList();
+
+let item = new Item("wash car", "go outside and wash car");
+
+firstList.addItem(item);
+
+console.log(firstList.items[0])
+firstList.items[0].done = true
+console.log(firstList.items[0])
